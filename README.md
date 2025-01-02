79730f7c1762845cbec7de7e130f57ce7b5fe3ac
Adam Balsam <balsam.adam@gmail.com>
2025-01-02 00:13:04 -0500

    README and favicon

---
+++ README.md
@@ -0,0 +1,37 @@
+#Set Checker
+https://en.wikipedia.org/wiki/Set_(card_game)
+
+Checks to see if collection contains a Set.
+
+##Usage
+Rudimentary Web interface deployed to https://sets-2febbc630b1d.herokuapp.com/
+
+###Developers
+1. Create a number of cards:
+   ```injectablephp
+   $card1 = new Card(Count::ONE, Color::RED, Fill::SOLID, Shape::SQUIGGLE);
+   $card2 = new Card(Count::ONE, Color::PURPLE, Fill::EMPTY, Shape::PILL);
+   $card3 = new Card(Count::ONE, Color::GREEN, Fill::SHADED, Shape::SQUIGGLE);
+   ...
+   ```
+2. Create a board from the cards:
+   ```injectablephp
+   $board = new \Balsama\Sets\Board([
+            $card1,
+            $card2,
+            $card3,
+            ...
+   ]);
+   ```
+3. Check to see if there are any sets!
+   ```injectablephp
+   $sets = $board->getSets();
+   ```
+   
+##About
+Inspired by this [Numberphile video](https://www.youtube.com/watch?v=EkFX9jUJPKk), I assign each attribute (count,
+color, fill, and shape) a value from 0-2 and then mod(3) the values from each attribute across each Collection of three
+cards in a board. If all attributes mod(3) == 0, it's a Set!
+
+I'm pretty sure this is the first practical use of modulo I've ever implementaed outside of /every n rows/ or similar
+:).
--- web/index.php
+++ web/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Set checker</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -27,7 +31,7 @@
 <div class="container my-5">
     <h1>Set checker!</h1>
     <div class="col-lg-8 px-0">
-        <p>Enter the cards currently on your table to check if there is a set.</p>
+        <p>Enter the cards currently on your table to check if there is a set (assumes there are 12 cards on your table).</p>
 
         <form action="submit.php" method="post">
             <?php $i = 1; while ($i < 13) { ?>
@@ -62,6 +66,7 @@
             <?php $i++; } ?>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+        <p align="right">Source code on <a href="https://github.com/balsama/sets">GitHub</a></p>
     </div>
 </div>
 
